5a8500d050b97876428fcbf8e1c6c3ff7a00f445
Amos Barbato <amosbarbato@gmail.com>
2025-01-14 12:51:13 -0300

    feat: create the crud routes controlllers and ensure the commands better flow

---
+++ src/controller/item-control.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+interface RequestBody {
+  title: string
+}
+
+const prisma = new PrismaClient()
+
+const safeResponse = async (res: Response, callback: () => Promise<any>) => {
+  try {
+    const data = await callback()
+    if (!res.headersSent) { res.json(data) }
+  } catch (error) {
+    console.error(error)
+    if (!res.headersSent) {
+      res.status(500).json({ error: 'Ocorreu um erro inesperado' })
+    }
+  }
+}
+
+const getItems = (req: Request, res: Response) => {
+  safeResponse(res, async () => {
+    return prisma.item.findMany()
+  })
+}
+
+const addItem = (req: Request<{}, {}, RequestBody>, res: Response) => {
+  safeResponse(res, async () => {
+    const { title } = req.body
+
+    await prisma.item.create({ data: { title } })
+
+    return prisma.item.findMany
+  })
+}
+
+const deleteItem = (req: Request, res: Response) => {
+  safeResponse(res, async () => {
+    const { id } = req.params
+    const itemId = parseInt(id, 10)
+
+    await prisma.item.findUnique({ where: { id: itemId } });
+
+    await prisma.item.delete({ where: { id: itemId } })
+
+    return prisma.item.findMany();
+  })
+}
+
+export { getItems, addItem, deleteItem }
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+import bodyParser from "body-parser"
+import { addItem, deleteItem, getItems } from "./controller/item-control"
+
 const express = require('express')
 const cors = require('cors')
 
@@ -6,7 +9,14 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.json())
 
+// routes
+app.get('/', getItems)
+app.post('/add', addItem)
+app.delete('/:id', deleteItem)
+
+// start the server
 const PORT = 5000
 app.listen(PORT, () => {
   console.log(`HTTP is running on port:${PORT}`)
